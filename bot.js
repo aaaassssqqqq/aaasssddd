67c1b8255c487b53e5a3b0e67c9feaa77080a6a8
fan33a <42098159+fan33a@users.noreply.github.com>
2018-09-03 21:02:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,4 +7,9 @@ client.on('ready', () => {
 client.on('ready', () => {
 client.user.setGame(`الكمبيوتر شغال 24 ساعة .`,'https://www.twitch.tv/v5bz');
 }); 
+client.on('message', msg => {
+  if (msg.content === 'هلا') {
+    msg.reply('أهلاً وسهلاً');
+  }
+});
 client.login(process.env.BOT_TOKEN);
