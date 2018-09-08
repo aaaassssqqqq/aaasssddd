bdf84b667000ab59680ba1155530c2dd4bc4eea2
fan33a <42098159+fan33a@users.noreply.github.com>
2018-09-08 15:16:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
   
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`); 
-}); 
+});
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
