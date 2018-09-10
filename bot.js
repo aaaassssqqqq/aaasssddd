67ff4b4d50c0fb42db345a2c2b7cc341e2eee93a
fan33a <42098159+fan33a@users.noreply.github.com>
2018-09-10 13:26:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
 });
 client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
  )});
