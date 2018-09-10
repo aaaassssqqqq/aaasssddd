573bb436b83400f855611a08d84219d24120d220
fan33a <42098159+fan33a@users.noreply.github.com>
2018-09-10 13:34:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,5 +14,26 @@ A7MD.on("guildMemberAdd", member => {
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
-})
+ if (message.content === "!help") {
+   message.react("😜")
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .addField("『Bot 1 | Arab Community』 『اسم البوت』", true)
+      
+      .addField("『مصمم البوت iTzAnan_Over#3412』", true)
+      
+      .addField("『!help^』😘『اذا تبي جميع الاوامر مع اوامر اضافية』😵", true)
+      
+	    .addField("『!help^^』👻『اذا تبي الاوامر العامة』😳", true)
+	    
+      .addField("『!help^^^』😮『اذا تبي  الاوامر الادارية』🤒", true)
+      
+      .addField("『🤑』『قريبا سوف نضيف المزيد و المزيد من الاوامر 』『😉』", true)
+      
+  message.channel.sendMessage({embed});
+
+ }
+});
+
+});
 client.login(process.env.BOT_TOKEN)
