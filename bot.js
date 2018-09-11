7d2d3d2e554de0398560fe67d590b26f2b5cff04
fan33a <42098159+fan33a@users.noreply.github.com>
2018-09-11 07:40:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,18 @@
-const Discord = require('discord.js');
-const A7MD = new Discord.Client();
-
-console.log("BOT ONLINE");
- 
-A7MD.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم 
-لو سمحت ممكن تدخل 
-
-                               [ https://discord.gg/JjEc4Tb ] 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
-}).catch(console.error)
- if (message.content === "!help") {
-   message.react("😜")
-  const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .addField("『Bot 1 | Arab Community』 『اسم البوت』", true)
-      
-      .addField("『مصمم البوت iTzAnan_Over#3412』", true)
-      
-      .addField("『!help^』😘『اذا تبي جميع الاوامر مع اوامر اضافية』😵", true)
-      
-	    .addField("『!help^^』👻『اذا تبي الاوامر العامة』😳", true)
-	    
-      .addField("『!help^^^』😮『اذا تبي  الاوامر الادارية』🤒", true)
-      
-      .addField("『🤑』『قريبا سوف نضيف المزيد و المزيد من الاوامر 』『😉』", true)
-      
-  message.channel.sendMessage({embed});
-
- }
+onst Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`.....');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
+client.on('message', message => {
+     if (message.content === ".servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
 
 });
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
